ab0900dae7001dd0e0c235c939d3321c200d483e
David Angeles <davidangelesam@gmail.com>
2026-08-10 12:16:37 -0500

    :art: configuracion de tailwind y limpieca css

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,10 @@ import { useState } from 'react'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-      <h1>Desserts page</h1>
+      <h1 className="text-red-500">Desserts page</h1>
     </>
   )
 }
