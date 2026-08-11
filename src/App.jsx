7368daa3faf1f08535eba034b3563dcc9353db30
David Angeles <davidangelesam@gmail.com>
2026-08-11 11:21:07 -0500

    :iphone: Vista movil

--- src/App.jsx
+++ src/App.jsx
@@ -13,7 +13,7 @@ function App() {
       <div className="grid grid-cols-1 lg:grid-cols-4 gap-8">
         <div className="lg:col-span-3">
           <Header />
-          <div className="grid grid-cols-1 lg:grid-cols-3 gap-6 mt-8">
+          <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6 mt-8">
             {dessertsData.map((dessert, index) => (
               <ProductCard 
                 key={dessert.name} 
