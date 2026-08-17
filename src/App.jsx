d5393261304d6bc65765681fb87554b96afe6a2b
David Angeles <davidangelesam@gmail.com>
2026-08-17 04:05:58 -0500

    :bug: Carpeta routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,13 @@
-import {Routes, Route} from 'react-router-dom'
-import Home from './pages/Home.jsx'
-import ProductDetails from './pages/ProductDetails.jsx'
-import NotFound from './pages/NotFound.jsx'
 import {toast, Toaster} from 'react-hot-toast';
-import AdminProductos from './pages/AdminProductos.jsx';
-import ProductForm from './pages/ProductForm.jsx';
+import AppRouter from './routes/AppRouter';
+
 
 export default function App() {
   return (
     <>
       <Toaster position="top-right" reverseOrder={false} />
 
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/product/:id" element={<ProductDetails />} />
-        <Route path="/admin" element={<AdminProductos />} />
-        <Route path="/admin/nuevo" element={<ProductForm />} />
-        <Route path="/admin/editar/:id" element={<ProductForm />} />
-        <Route path="*" element={<NotFound />} />
-      </Routes>
+      <AppRouter/>
     </>
   )
 }
